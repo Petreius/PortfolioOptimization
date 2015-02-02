bb846657dc1904fff67d6f7c28ea0d687a1c010e
thomasdoutre <tom.doutre@gmail.com>
2015-02-02 15:24:28 +0100

    Base apres discussion avec PA

--- Main.java
+++ Main.java
@@ -1,20 +1,8 @@
-import java.io.IOException;
-
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		
-		MeanVarianceParser mvp = new MeanVarianceParser("/Users/thomasdoutre/Desktop/port1.txt");
-		mvp.donneMatriceMoyenneVariance();
-		
-		double[][] cov = mvp.matriceCovariance;
-		double[] moy = mvp.vecteurMoyennes;
-		double[] var = mvp.vecteurVariances;
-		
-		Recuit.solution(moy, var, cov);
-		
-		//Recuit.solution();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
 	}
 
