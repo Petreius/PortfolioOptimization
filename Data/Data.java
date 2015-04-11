5768e7488baabf516fb8b4661a4bdfe762bb4b5d
Petreius <pabelaube@hotmail.fr>
2015-04-11 20:16:47 +0200

    Update Data.java
    - AutoScaling : adaptation de la fenetre d'observation à l'actif sur lequel on a le moins de données pour éviter des comportements imprevus... (prix nuls etc)

--- Data/Data.java
+++ Data/Data.java
@@ -3,6 +3,7 @@ package Data;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import YahooFinance.*;
 
@@ -13,6 +14,8 @@ public class Data {
 	private double[][] rawReturnsMatrix;
 
 	public Data(ArrayList<YahooFinanceHttp> tickersList){
+		tickersList = AutoScaling(tickersList);
+		System.out.println("nouvelle de date de début : "+tickersList.get(0).getDate().get(0));
 		double[][] quoteMatrix = new double[tickersList.get(0).getAdjClose().size()][tickersList.size()];
 		for(int i = 0; i < tickersList.size(); i++){
 			for(int j = 0; j < tickersList.get(i).getAdjClose().size(); j++){
@@ -24,14 +27,42 @@ public class Data {
 		this.rawReturnsMatrix = computeRawReturns(quoteMatrix);
 	}
 
+	// Pour eviter tout comportement indetermine, AutoScaling adapte la fenetre d observation
+	public ArrayList<YahooFinanceHttp> AutoScaling(ArrayList<YahooFinanceHttp> tickersList){
+		ArrayList<Integer> timeLength = new ArrayList<Integer>();
+		int minLength = 0;
+		for(int i = 0; i < tickersList.size(); i++){
+			timeLength.add(tickersList.get(i).getDate().size());
+		}
+		minLength = (int) Collections.min(timeLength);
+		for(int i = 0; i < tickersList.size(); i++){
+			int length = tickersList.get(i).getDate().size();
+			for(int j = 0; j < length-minLength; j++){
+				tickersList.get(i).getDate().remove(0);
+				tickersList.get(i).getAdjClose().remove(0);
+				tickersList.get(i).getVolume().remove(0);
+				tickersList.get(i).getOpen().remove(0);
+				tickersList.get(i).getClose().remove(0);
+				tickersList.get(i).getHigh().remove(0);
+				tickersList.get(i).getLow().remove(0);
+			}
+		}
+		return tickersList;
+	}
+
 	private double[][] computeLogReturns(double[][] quoteMatrix){
 		double[][] logReturnsMatrix = new double[quoteMatrix.length-1][quoteMatrix[0].length];
+		int warningNullPrices = -1;
 		for(int j = 0; j < quoteMatrix[0].length; j++){
 			for(int i = 0; i < quoteMatrix.length-1; i++){
 				if(quoteMatrix[i+1][j] != 0.0 && quoteMatrix[i][j] != 0.0){
 					logReturnsMatrix[i][j] = Round(Math.log(quoteMatrix[i+1][j]/quoteMatrix[i][j]),4);
 				}
 				else{ 
+					if(warningNullPrices != j){
+						System.out.println("ATTENTION certaines cotations sont nulles sur cet intervalle de temps pour le ticker numero "+j);
+						warningNullPrices = j;
+					}
 					logReturnsMatrix[i][j] = 0.0;
 				}
 			}
