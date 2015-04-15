3ce69d3e0cf138f3bef2d54ea5d5adc64b246e67
Petreius <pabelaube@hotmail.fr>
2015-04-15 15:07:06 +0200

    Create Mutation.java

---
+++ Mutation/Mutation.java
@@ -0,0 +1,32 @@
+package Recuit;
+
+import Data.*;
+
+public class Mutation {
+	
+	public static double[] buyAndSell(double[] weights){
+		
+		double step = 0.15; //Pour l'instant
+		
+		int soldAsset = randomInt(0,weights.length-1);
+		int boughtAsset = randomIntWithException(0,weights.length-1,soldAsset);
+		weights[soldAsset] = Data.Round(weights[soldAsset] - step,4);
+		weights[boughtAsset] = Data.Round(weights[boughtAsset] + step,4);
+		return weights;
+	}
+
+	public static int randomInt (int deb, int fin){
+		
+		return (int)(Math.random()*(fin-deb+1)+deb);
+		
+	}
+	
+public static int randomIntWithException (int deb, int fin, int i){
+		int random = randomInt(deb,fin);
+		while(random == i){
+		random = randomInt(deb,fin);
+		}
+		return random;
+	}
+
+}
