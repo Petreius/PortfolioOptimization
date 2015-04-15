861c87a2005695c8aa8f4763961a04c486609806
Petreius <pabelaube@hotmail.fr>
2015-04-15 15:04:47 +0200

    Update Portfolio.java

--- Data/Portfolio.java
+++ Data/Portfolio.java
@@ -1,12 +1,15 @@
 package Data;
 
-public class Portfolio {
-	private double[] weights;
+import org.apache.commons.math3.stat.descriptive.rank.Percentile;
+
+public class Portfolio{
+	static private double[] weights;
 	private double[] expectedLogReturn;
 	private double[] expectedRawReturn;
-
+	private double valueAtRisk;
+	
 	public Portfolio(Data data){
-
+		Data dataSet= data;
 		double[] weights = new double[data.getMQuotes()[0].length];
 		// TODO double[] expectedLogReturn = new double[data.getMQuotes().length];
 
@@ -23,8 +26,20 @@ public class Portfolio {
 		this.expectedRawReturn = computeRawReturn(data, weights);
 		// On calcule les LogReturns du portefeuille initial
 		this.expectedLogReturn = computeLogReturn(data, weights);
+		// Calcul de la VAR avec les RawReturns
+		this.valueAtRisk = new ValueAtRisk(this.expectedRawReturn, 5.0).getVARValue();
 	}
-
+	
+	public Portfolio(double[] pourcentages){
+		this.setWeights(pourcentages);
+		//A MODIFIER SELON LE CALCUL DE LA VAR
+	}
+	
+	public Portfolio clone(){
+		Portfolio q = new Portfolio(this.getWeights());
+		return q;
+	}
+	
 	public double[] computeLogReturn(Data data, double[] weights){
 		double expectedLogReturn[] = new double[data.getMQuotes().length];
 		double dayAValue = 0.0;
@@ -58,9 +73,14 @@ public class Portfolio {
 		}
 		return expectedRawReturn;
 	}
-
+	
+	public double getEnergy(){
+		ValueAtRisk valueAtRisk = new ValueAtRisk(this.expectedRawReturn, 5.0);
+		return valueAtRisk.getVARValue();
+	}
+	
 	public double[] getWeights(){
-		return this.weights;
+		return Portfolio.weights;
 	}
 
 	public double[] getExpectedRawReturn(){
@@ -69,5 +89,38 @@ public class Portfolio {
 	
 	public double[] getExpectedLogReturn(){
 		return this.expectedLogReturn;
+	}
+	
+	public double getValueAtRisk(){
+		return this.valueAtRisk;
+	}
+	
+	public void setWeights(double[] weights) {
+		Portfolio.weights = weights;
+	}
+	
+	public void setExpectedRawReturn(Data data, double[] weights){
+		this.expectedRawReturn = computeRawReturn(data, weights);
+	}
+	
+	public void setExpectedLogReturn(Data data, double[] weights){
+		this.expectedLogReturn = computeLogReturn(data, weights);
+	}
+	
+	public void setValueAtRisk(double[] expectedReturn){
+		Percentile percentile = new Percentile();
+		this.valueAtRisk = Data.Round(percentile.evaluate(expectedReturn, 5.0),4);
+	}
+	
+	public String toString(){
+		String strWeights = "[";
+		for(int i=0; i < this.weights.length; i++){
+			if(i != this.weights.length-1){
+			strWeights += this.weights[i]+",";
+			} else{
+				strWeights += this.weights[this.weights.length-1]+"]";
+			}
+		}
+		return strWeights;
 	}
 }
