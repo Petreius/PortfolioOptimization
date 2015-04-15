7bfd3b19a368fb40c2411db5c7db79d0557bc2b1
Petreius <pabelaube@hotmail.fr>
2015-04-15 15:03:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,21 +4,22 @@ import java.util.Calendar;
 import YahooFinance.YahooFinanceHttp;
 import YahooFinance.YahooFinanceURLConstructor;
 import Data.*;
+import Recuit.*;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws CloneNotSupportedException{
  
         Calendar startDate = Calendar.getInstance();
         //Date de debut
-        startDate.set(Calendar.YEAR, 2015);
-        startDate.set(Calendar.MONTH,3); //Mois Jan = 0, Fev = 1...Dec = 11
-        startDate.set(Calendar.DATE, 6);
+        startDate.set(Calendar.YEAR, 2014);
+        startDate.set(Calendar.MONTH,12); //Mois Jan = 0, Fev = 1...Dec = 11
+        startDate.set(Calendar.DATE, 14);
  
         Calendar endDate = Calendar.getInstance();
         //Date de fin
         endDate.set(Calendar.YEAR, 2015);
         endDate.set(Calendar.MONTH, 3);
-        endDate.set(Calendar.DATE, 8);
+        endDate.set(Calendar.DATE, 14);
  
         // La tickersList contient l'ensemble des tickers que nos considérons et leur prix respectif sur la fenetre d'observation
         ArrayList<YahooFinanceHttp> tickersList = new ArrayList<YahooFinanceHttp>();
@@ -27,6 +28,9 @@ public class Main {
         YahooFinanceURLConstructor AAPL_URL = new YahooFinanceURLConstructor(startDate, endDate, "AAPL");
         YahooFinanceURLConstructor GOOG_URL = new YahooFinanceURLConstructor(startDate, endDate, "GOOG");
         YahooFinanceURLConstructor MSFT_URL = new YahooFinanceURLConstructor(startDate, endDate, "MSFT");
+        YahooFinanceURLConstructor BNPPA_URL = new YahooFinanceURLConstructor(startDate, endDate, "BNP.PA");
+        YahooFinanceURLConstructor ORPA_URL = new YahooFinanceURLConstructor(startDate, endDate, "OR.PA");
+        YahooFinanceURLConstructor EFRPA_URL = new YahooFinanceURLConstructor(startDate, endDate, "FR.PA");
         
         System.out.println("Interroge Yahoo Finance...");
         
@@ -34,6 +38,9 @@ public class Main {
         YahooFinanceHttp AAPL = new YahooFinanceHttp(AAPL_URL.constructURL()); 
         YahooFinanceHttp GOOG = new YahooFinanceHttp(GOOG_URL.constructURL());
         YahooFinanceHttp MSFT = new YahooFinanceHttp(MSFT_URL.constructURL());
+        YahooFinanceHttp BNPPA = new YahooFinanceHttp(BNPPA_URL.constructURL());
+        YahooFinanceHttp ORPA = new YahooFinanceHttp(ORPA_URL.constructURL());
+        YahooFinanceHttp EFRPA = new YahooFinanceHttp(EFRPA_URL.constructURL());
         
         System.out.println("Donnees recuperees avec succes");
         
@@ -41,6 +48,9 @@ public class Main {
         tickersList.add(AAPL);
         tickersList.add(GOOG);
         tickersList.add(MSFT);
+        tickersList.add(BNPPA);
+        tickersList.add(ORPA);
+        tickersList.add(EFRPA);
         
         // Data est un objet qui contient la matrice des prix et la matrice des log returns obtenues à partir de la tickersList
         Data data = new Data(tickersList);
@@ -49,19 +59,23 @@ public class Main {
         Portfolio portfolio = new Portfolio(data);
         
         // Creation d'une valueAtRisk historique
-        ValueAtRisk valueAtRisk = new ValueAtRisk(portfolio.getExpectedRawReturn(),5.0);
+        //ValueAtRisk valueAtRisk = new ValueAtRisk(portfolio.getExpectedRawReturn(),5.0);
        
         // Test
-    	System.out.println("Voici les prix Apple pour ces 3 jours: "+data.getMQuotes()[0][0]+" , "+data.getMQuotes()[1][0]+" , "+data.getMQuotes()[2][0]);
-    	System.out.println("Voici les retours associes: "+data.getMRawReturns()[0][0]+" , "+data.getMRawReturns()[1][0]);
-    	System.out.println("Voici les prix Google pour ces 3 jours: "+data.getMQuotes()[0][1]+" , "+data.getMQuotes()[1][1]+" , "+data.getMQuotes()[2][1]);
-    	System.out.println("Voici les retours associes: "+data.getMRawReturns()[0][1]+" , "+data.getMRawReturns()[1][1]);
-    	System.out.println("Voici les prix Microsoft pour ces 3 jours: "+data.getMQuotes()[0][2]+" , "+data.getMQuotes()[1][2]+" , "+data.getMQuotes()[2][2]);
-    	System.out.println("Voici les retours associes: "+data.getMRawReturns()[0][2]+" , "+data.getMRawReturns()[1][2]);
-    	System.out.println("Voici la repartition de votre portefeuille: "+portfolio.getWeights()[0]+" , "+portfolio.getWeights()[1]+" , "+portfolio.getWeights()[2]);
-    	System.out.println("Les retours de votre portefeuille sont donc: "+portfolio.getExpectedRawReturn()[0]+" , "+portfolio.getExpectedRawReturn()[1]);
-    	System.out.println("Les retours logarithmiques de votre portefeuille: "+portfolio.getExpectedLogReturn()[0]+" , "+portfolio.getExpectedLogReturn()[1]);
-    	System.out.println("La V@R de votre portefeuille est "+valueAtRisk.getValueAtRisk());
+    	System.out.println("repartition initiale" + portfolio.toString());
+    	Portfolio clonePortfolio = portfolio.clone();
+    	
+    	System.out.println("repartition initiale du clone" + clonePortfolio.toString());
+    	System.out.println("La V@R initiale de votre portefeuille est "+portfolio.getValueAtRisk());
+    	System.out.println("La V@R initiale de votre portefeuille clone est "+clonePortfolio.getValueAtRisk());
+    	
+    	double[] tableau = Mutation.buyAndSell(portfolio.getWeights());
+    	portfolio.setWeights(tableau);
+    	//portfolio = Mutation.buyAndSell(portfolio);
+    	System.out.println("repartition finale" + portfolio.toString());
+    	System.out.println("repartition finale du putain de clone " + clonePortfolio.toString());
+
+    	//Portfolio solution = Recuit.solution(data);
     }
  
 }
