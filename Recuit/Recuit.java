121905183bc411204df753d1dba655772b210786
Petreius <pabelaube@hotmail.fr>
2015-04-20 22:07:42 +0200

    Update Recuit.java

--- Recuit/Recuit.java
+++ Recuit/Recuit.java
@@ -6,18 +6,17 @@ import Data.*;
 public class Recuit {
 
 	static double temperature = 100;
-	static double refroidissement = 0.001;
+	static double refroidissement = 0.0001;
 	
 	public static double probaAcceptation(double currentEnergy, double newEnergy, double temperature)
 	{
 		if (newEnergy < currentEnergy) {
 			return 1.0;
 		}
-		//return 0;
-		return Math.exp((currentEnergy - newEnergy) / (10000000*temperature));
+		return Math.exp((currentEnergy - newEnergy) / ((temperature)));
 	}
 	
-	public static void solution(Data data)
+	public Portfolio solution(Data data, double target)
 	{
 		
 		Portfolio currentPortfolio = new Portfolio(data);
@@ -25,26 +24,28 @@ public class Recuit {
 		Portfolio newPortfolio = currentPortfolio.clone();
 		
 		double temperatureRecuit = temperature;
-		
 		while (temperatureRecuit > 1) {
 			
 			Mutation.buyAndSell(newPortfolio);
 			newPortfolio.update(data);
 			
-			double currentEnergy = currentPortfolio.getEnergy();
-			double newEnergy = newPortfolio.getEnergy();
+			double currentEnergy = currentPortfolio.getEnergy(target);
+			double newEnergy = newPortfolio.getEnergy(target);
+			double pr = probaAcceptation(currentEnergy, newEnergy, temperatureRecuit);
 			
-			if (probaAcceptation(currentEnergy, newEnergy, temperatureRecuit) >= Math.random()) {
+			if (pr >= Math.random()) {
 				currentPortfolio = newPortfolio.clone(); 
 			}
 
-			if (currentPortfolio.getEnergy() < bestPortfolio.getEnergy()) {
+			if (currentPortfolio.getEnergy(target) < bestPortfolio.getEnergy(target)) {
 				bestPortfolio = currentPortfolio.clone();
 			}
 			temperatureRecuit -= refroidissement;
 		}
 		
 		System.out.println("portefeuille final \n" + bestPortfolio.toString());
+		return bestPortfolio;
 	}
 
 }
+
